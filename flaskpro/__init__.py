a9c45cef332ee7afe4afc7f4a5c8344763ecb86a
kevinkurian7 <kevinthathathil@gmail.com>
2023-03-23 00:36:19 +0530

    search auto

--- flaskpro/__init__.py
+++ flaskpro/__init__.py
@@ -1,6 +1,6 @@
 import os
-
-from flask import Flask,render_template
+import requests
+from flask import Flask,render_template,request, jsonify
 import warnings
 warnings.filterwarnings("ignore")
 import os
@@ -11,12 +11,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/search_page')
+def search_page():
+    return render_template('search_page.html')   
+
 @app.route('/search')
 def search():
-    return render_template('search.html')   
-@app.route('/about')
-def about():
-    return render_template('about.html')       
+ 
+    query = request.args.get('query')
+    url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={query}&apikey=9HE15L51KC2VW3UQ'
+    response = requests.get(url)
+    data = response.json()
+    results = []
+    try:
+        for item in data['bestMatches']:
+            result = {
+                'symbol': item['1. symbol'],
+                'name': item['2. name']
+            }
+            results.append(result)
+        return jsonify(results)
+    except:
+        return jsonify(results)
+
+        
 
 if __name__ == '__main__':
     app.run()
