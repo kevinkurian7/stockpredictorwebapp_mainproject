34cb9855060536286170037f10e1e3ff244fc6d6
kevinkurian7 <kevinthathathil@gmail.com>
2023-04-13 11:11:44 +0530

    added ticker

--- flaskpro/__init__.py
+++ flaskpro/__init__.py
@@ -75,7 +75,7 @@ def results():
 
 
 
-    # do something with query
+    # query
     def get_historical(quote):
         end = datetime.now()
         start = datetime(end.year-2,end.month,end.day)
@@ -85,11 +85,10 @@ def results():
         if(df.empty):
             ts = TimeSeries(key='9HE15L51KC2VW3UQ',output_format='pandas')
             data, meta_data = ts.get_daily_adjusted(symbol=quote, outputsize='full')
-            #Format df
-            #Last 2 yrs rows => 502, in ascending order => ::-1
+            
             data=data.head(503).iloc[::-1]
             data=data.reset_index()
-            #Keep Required cols only
+           
             df=pd.DataFrame()
             df['Date']=data['date']
             df['Open']=data['1. open']
@@ -108,22 +107,14 @@ def results():
         #Split data into training set and test set
         dataset_train=df.iloc[0:int(0.8*len(df)),:]
         dataset_test=df.iloc[int(0.8*len(df)):,:]
-        ############# NOTE #################
-        #TO PREDICT STOCK PRICES OF NEXT N DAYS, STORE PREVIOUS N DAYS IN MEMORY WHILE TRAINING
-        # HERE N=7
-        ###dataset_train=pd.read_csv('Google_Stock_Price_Train.csv')
-        training_set=df.iloc[:,4:5].values# 1:2, to store as numpy array else Series obj will be stored
-        #select cols using above manner to select as float64 type, view in var explorer
-
-        #Feature Scaling
+       
+        training_set=df.iloc[:,4:5].values
+   
         from sklearn.preprocessing import MinMaxScaler
         sc=MinMaxScaler(feature_range=(0,1))#Scaled values btween 0,1
         training_set_scaled=sc.fit_transform(training_set)
-        #In scaling, fit_transform for training, transform for test
-        
-        #Creating data stucture with 7 timesteps and 1 output. 
-        #7 timesteps meaning storing trends from 7 days before current day to predict 1 next output
-        X_train=[]#memory with 7 days from day i
+      
+        X_train=[]
         y_train=[]#day i
         for i in range(7,len(training_set_scaled)):
             X_train.append(training_set_scaled[i-7:i,0])
@@ -133,77 +124,67 @@ def results():
         y_train=np.array(y_train)
         X_forecast=np.array(X_train[-1,1:])
         X_forecast=np.append(X_forecast,y_train[-1])
-        #Reshaping: Adding 3rd dimension
-        X_train=np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))#.shape 0=row,1=col
+    
+        X_train=np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))
         X_forecast=np.reshape(X_forecast, (1,X_forecast.shape[0],1))
-        #For X_train=np.reshape(no. of rows/samples, timesteps, no. of cols/features)
-        
-        #Building RNN
+       
+    
         from keras.models import Sequential
         from keras.layers import Dense
         from keras.layers import Dropout
         from keras.layers import LSTM
-        
-        #Initialise RNN
+       
         regressor=Sequential()
         
-        #Add first LSTM layer
+        
         regressor.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
-        #units=no. of neurons in layer
-        #input_shape=(timesteps,no. of cols/features)
-        #return_seq=True for sending recc memory. For last layer, retrun_seq=False since end of the line
+
         regressor.add(Dropout(0.1))
         
-        #Add 2nd LSTM layer
+       
         regressor.add(LSTM(units=50,return_sequences=True))
         regressor.add(Dropout(0.1))
         
-        #Add 3rd LSTM layer
+     
         regressor.add(LSTM(units=50,return_sequences=True))
         regressor.add(Dropout(0.1))
         
-        #Add 4th LSTM layer
+     
         regressor.add(LSTM(units=50))
         regressor.add(Dropout(0.1))
         
-        #Add o/p layer
+   
         regressor.add(Dense(units=1))
         
-        #Compile
+     
         regressor.compile(optimizer='adam',loss='mean_squared_error')
         
         #Training
         regressor.fit(X_train,y_train,epochs=25,batch_size=32 )
-        #For lstm, batch_size=power of 2
         
-        #Testing
-        ###dataset_test=pd.read_csv('Google_Stock_Price_Test.csv')
         real_stock_price=dataset_test.iloc[:,4:5].values
         
-        #To predict, we need stock prices of 7 days before the test set
-        #So combine train and test set to get the entire data set
+        
         dataset_total=pd.concat((dataset_train['Close'],dataset_test['Close']),axis=0) 
         testing_set=dataset_total[ len(dataset_total) -len(dataset_test) -7: ].values
         testing_set=testing_set.reshape(-1,1)
-        #-1=till last row, (-1,1)=>(80,1). otherwise only (80,0)
         
-        #Feature scaling
+        
+       
         testing_set=sc.transform(testing_set)
-        
-        #Create data structure
+       
         X_test=[]
         for i in range(7,len(testing_set)):
             X_test.append(testing_set[i-7:i,0])
-            #Convert list to numpy arrays
+          
         X_test=np.array(X_test)
         
-        #Reshaping: Adding 3rd dimension
+       
         X_test=np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
         
-        #Testing Prediction
+
         predicted_stock_price=regressor.predict(X_test)
         
-        #Getting original prices back from scaled values
         predicted_stock_price=sc.inverse_transform(predicted_stock_price)
         fig = plt.figure(figsize=(7.2,4.8),dpi=65)
         plt.plot(real_stock_price,label='Actual Price')  
@@ -216,11 +197,10 @@ def results():
         
         error_lstm = math.sqrt(mean_squared_error(real_stock_price, predicted_stock_price))
         
-        
-        #Forecasting Prediction
+
         forecasted_stock_price=regressor.predict(X_forecast)
         
-        #Getting original prices back from scaled values
+        
         forecasted_stock_price=sc.inverse_transform(forecasted_stock_price)
         
         lstm_pred=forecasted_stock_price[0,0]
@@ -230,8 +210,6 @@ def results():
         print("LSTM RMSE:",error_lstm)
         print("##############################################################################")
         return lstm_pred,error_lstm
-#arima
-    #******************** ARIMA SECTION ********************
 
     
 
@@ -275,17 +253,7 @@ def results():
 
 
 
-'''def ticker(request):
-    # ================================================= Load Ticker Table ================================================
-    ticker_df = pd.read_csv('app/Data/new_tickers.csv') 
-    json_ticker = ticker_df.reset_index().to_json(orient ='records')
-    ticker_list = []
-    ticker_list = json.loads(json_ticker)
 
-
-    return render(request, 'ticker.html', {
-        'ticker_list': ticker_list
-    })'''
 @app.route('/ticker')
 def ticker():
     with open('flaskpro/templates/ticker.csv', 'r') as f:
